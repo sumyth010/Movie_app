d4cd2fa7b6be0abf2d701e7d7889c258f16962e1
sumyth010 <sumyth01@gmail.com>
2025-08-28 19:19:49 +0545

    Added some style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
         return (
             <ImageBackground
                 source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image source={icon} tintColor="#151312" className="size-5" />
                 <Text className="text-secondary text-base font-semibold">{title}</Text>
@@ -29,7 +29,29 @@ const TabIcon = ({ focused, icon, title }: any) => {
 
 const _layout = () => {
     return (
-        <Tabs>
+        <Tabs 
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36, 
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1 ,
+                borderColor: '#OfOd23'
+
+            }
+        }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
---
+++ components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const SearchBar = () => {
+  return (
+    <View className="flex-row item-center bg-dark-200 rounded-full px-5 py-4">
+    
+    </View>
+  )
+}
+
+export default SearchBar
